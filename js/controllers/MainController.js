2d460f8db4a90b305ede9906cb29fc51ad9e5e47
vmccreedy <virginiamcc@users.noreply.github.com>
2017-09-24 08:58:25 -0700

    Initial commit, push to GitHub.

---
+++ js/controllers/MainController.js
@@ -0,0 +1,39 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+  $scope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marinara sauce, mozzarella, fresh basil.',
+      price: 6.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+      price: 7.95
+    },
+    {
+      name: 'Rustica',
+      description: 'Marinara sauce, olives, roasted garlic, fresh oregano.',
+      price: 8.95
+    }
+  ];
+
+}]);
